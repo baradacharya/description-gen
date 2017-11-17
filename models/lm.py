82438e2ec7cd3f88af2dd7c47721e618ddc9c5e7
barada <barada.p.acharya@gmail.com>
2017-11-17 02:05:57 -0800

    update lm file

--- models/lm.py
+++ models/lm.py
@@ -10,12 +10,13 @@ import sys
 
 # Python 3 backwards compatibility tricks
 if sys.version_info.major > 2:
-
     def xrange(*args, **kwargs):
         return iter(range(*args, **kwargs))
 
+
     def unicode(*args, **kwargs):
         return str(*args, **kwargs)
+
 
 class LangModel:
     def fit_corpus(self, corpus): pass
@@ -32,23 +33,28 @@ class LangModel:
         num_words = 0.0
         sum_logprob = 0.0
         for s in corpus:
-            #num_words += len(s) + 1 #1 EOS
-            num_words += len(s)  # 2 SOS, 1 EOS
+            num_words += len(
+                s) - 2  # sentence involved 2 sos and 1 eos so remove 2 for sos which is not involved in prob calculation.
             sum_logprob += self.logprob_sentence(s)
-        return -(1.0/num_words)*(sum_logprob)
+        return -(1.0 / num_words) * (sum_logprob)
 
     def logprob_sentence(self, sentence): pass
+
     # required, update the model when a sentence is observed
     def fit_sentence(self, sentence): pass
+
     # optional, if there are any post-training steps (such as normalizing probabilities)
     def norm(self): pass
+
     # required, return the log2 of the conditional prob of word, given previous words
     def cond_logprob(self, word, previous): pass
+
     # required, the list of words the language model suports (including EOS)
     def vocab(self): pass
 
+
 class Unigram(LangModel):
-    def __init__(self, backoff = 0.000001):
+    def __init__(self, backoff=0.000001):
         self.model = dict()
         self.lbackoff = log(backoff, 2)
 
@@ -74,7 +80,7 @@ class Unigram(LangModel):
             self.model[w] = 1.0
 
     def fit_sentence(self, sentence):
-        #create a word dictionary with word and it's frequencies.
+        # create a word dictionary with word and it's frequencies.
         for w in sentence:
             self.inc_word(w)
         self.inc_word('<EOS>')
@@ -84,7 +90,7 @@ class Unigram(LangModel):
         """
         log(#freq(W_i)/#total words) , self.model[word] = log(#freq(W_i) - log(#total words)
         """
-        tot = 0.0 #total no of words
+        tot = 0.0  # total no of words
         for word in self.model:
             tot += self.model[word]
         ltot = log(tot, 2)
@@ -92,14 +98,15 @@ class Unigram(LangModel):
             self.model[word] = log(self.model[word], 2) - ltot
 
     def cond_logprob(self, word, previous):
-        #P(A|B) = #freq(W_i)/#total words, we have already normalized and working in log space
+        # P(A|B) = #freq(W_i)/#total words, we have already normalized and working in log space
         if word in self.model:
             return self.model[word]
         else:
-            return self.lbackoff #when the word not present log(0) = -inf, instead of that backoff
+            return self.lbackoff  # when the word not present log(0) = -inf, instead of that backoff
 
     def vocab(self):
         return self.model.keys()
+
 
 class Bigram(LangModel):
     def __init__(self, backoff=0.000001):
@@ -116,7 +123,6 @@ class Bigram(LangModel):
         The corpus consists of a list of sentences."""
         for s in corpus:
             self.fit_sentence(s)
-            # self.norm()
 
     def inc_uni(self, word):
         if word in self.unigram:
@@ -142,7 +148,6 @@ class Bigram(LangModel):
         for i in range(1, len(sentence)):
 
             bi = sentence[i - 1] + " " + sentence[i]
-            bi = bi.lower()
             if bi in self.bigram:
                 self.bigram[bi] += 1
             else:
@@ -150,15 +155,12 @@ class Bigram(LangModel):
         # for unigram
         # should add sos to unigram in this case to get p(x|sos) but remove sos while sampleing becoz we don't want p(sos|x)
         for i in range(len(sentence)):
-            self.inc_uni(sentence[i].lower())
+            self.inc_uni(sentence[i])
 
     def cond_logprob(self, word, previous):
 
         V = previous[-1]
         VW = previous[-1] + " " + word
-
-        V = V.lower()
-        VW = VW.lower()
 
         if VW in self.bigram:
             C_vw = self.bigram[VW]
@@ -173,41 +175,33 @@ class Bigram(LangModel):
         total_word = len(self.vocab())
         l_v = self.l * total_word
 
-        if l_v == 0:
-            if C_v == 0 and C_vw == 0:
-                score = 0
-            elif C_vw == 0:
-                score = self.lbackoff - log((C_v + l_v), 2)
-            elif C_v == 0:
-                score = log((C_vw + self.l), 2) - self.lbackoff
-            else:
-                score = log(C_vw, 2) - log(C_v, 2)
+        if l_v == 0 and C_vw == 0:
+            return self.lbackoff
         else:
-            score = log((C_vw + self.l), 2) - log((C_v + l_v), 2)
-
-        return score
+            return log((C_vw + self.l), 2) - log((C_v + l_v), 2)
 
     def vocab(self):
         return self.unigram.keys()
 
     def pre_processes(self, corpus):
-        # add <sos>, <sos>, and <eos> to sentence
+        # add <sos>, and <eos> to sentence
         for s in corpus:
             s.insert(0, '<SOS>')
             s.append("<EOS>")
 
+
 class Trigram(LangModel):
-    def __init__(self, backoff = 0.000001):
+    def __init__(self, backoff=0.000001):
         self.lbackoff = log(backoff, 2)
 
         self.trigram = dict()
         self.bigram = dict()
         self.unigram = dict()
 
-        #smoothing index
-        self.l = 0 #laplace
+        # smoothing index
+        self.l = 0  # laplace
 
-        #linerar_interpolation
+        # linerar_interpolation
         self.l1 = 0.20
         self.l2 = 0.39
         self.l3 = 0.41
@@ -218,7 +212,6 @@ class Trigram(LangModel):
         The corpus consists of a list of sentences."""
         for s in corpus:
             self.fit_sentence(s)
-        #self.norm()
 
     def inc_uni(self, word):
         if word in self.unigram:
@@ -227,16 +220,16 @@ class Trigram(LangModel):
             self.unigram[word] = 1
 
     def logprob_sentence(self, sentence):
-        #P(s_i) = product(cond prob) #so adding in log space
+        # P(s_i) = product(cond prob) #so adding in log space
         p = 0.0
-        for i in xrange(2,len(sentence)):
-            #log prob of sentences
-            #p += self.cond_logprob_linerar_interpolation(sentence[i], sentence[:i])
+        for i in xrange(2, len(sentence)):
+            # log prob of sentences
+            # p += self.cond_logprob_linerar_interpolation(sentence[i], sentence[:i])
             p += self.cond_logprob_laplace(sentence[i], sentence[:i])
         return p
 
     def fit_sentence(self, sentence):
-        #add bigram as sos + word and trigram as sos + sos + word
+        # add bigram as sos + word and trigram as sos + sos + word
 
         sentence.insert(0, '<SOS>')
         sentence.insert(0, '<SOS>')
@@ -245,8 +238,7 @@ class Trigram(LangModel):
         # for trigram
         for i in range(2, len(sentence)):
 
-            tri = sentence[i -2] + " " + sentence[i - 1] + " " + sentence[i]
-            tri = tri.lower()
+            tri = sentence[i - 2] + " " + sentence[i - 1] + " " + sentence[i]
             if tri in self.trigram:
                 self.trigram[tri] += 1
             else:
@@ -255,16 +247,14 @@ class Trigram(LangModel):
         # for bigram
         for i in range(1, len(sentence)):
 
-            bi = sentence[i - 1] +  " " + sentence[i]
-            bi = bi.lower()
+            bi = sentence[i - 1] + " " + sentence[i]
             if bi in self.bigram:
                 self.bigram[bi] += 1
             else:
                 self.bigram[bi] = 1
-        #for unigram
-        for i in range(2,len(sentence)):
-            self.inc_uni(sentence[i].lower())
-        #self.inc_uni('<EOS>')
+        # for unigram
+        for i in range(2, len(sentence)):
+            self.inc_uni(sentence[i])
 
     def cond_logprob_linerar_interpolation(self, word, previous):
 
@@ -274,55 +264,56 @@ class Trigram(LangModel):
         VW = previous[-1] + " " + word
         UVW = previous[-2] + " " + previous[-1] + " " + word
 
-        V = V.lower()
-        W = W.lower()
-        UV = UV.lower()
-        VW = VW.lower()
-        UVW = UVW.lower()
-
-        C_uvw = 1
         if UVW in self.trigram:
             C_uvw = self.trigram[UVW]
+        else:
+            C_uvw = 0
 
-        C_v = 1
-        if V in self.unigram:
-            C_v = self.unigram[V]
-
-        C_uv = 1
         if UV in self.bigram:
             C_uv = self.bigram[UV]
+        else:
+            C_uv = 0
 
-        C_vw = 1
         if VW in self.bigram:
             C_vw = self.bigram[VW]
+        else:
+            C_vw = 0
 
-        C_w = 1
+        if V in self.unigram:
+            C_v = self.unigram[V]
+        else:
+            C_v = 0
 
         if W in self.unigram:
             C_w = self.unigram[W]
+        else:
+            C_w = 0
 
         total_word = len(self.vocab())
 
-        """
-        q(w_i|W_i-2, w_i-1) = l1 * q_ml(w_i|W_i-2, w_i-1) + l2 * q_ml(w_i|w_i-1)  + l3 * q_ml(w_i)
-        = (l1 * C(u,v,w) * C(v) * total_word + l2 C(v,w) * C(u,v) * total_word + l3 * C(w) * C(u,v) * C(v))/ C(u,v) * C(v) * total_word
-        put these values
-        q_ml(w_i|W_i-2, w_i-1) = C(u,v,w) /C(u,v) 
-        q_ml(w_i|w_i-1) = C(v,w) /C(v)
-        q_ml(w_i) = C(w) /D (total_word)
-        """
-        return log(self.l1 * C_uvw * C_v * total_word + self.l2 * C_vw * C_uv * total_word + self.l3 * C_w * C_uv * C_v, 2) \
-               - log(C_uv, 2) - log(C_v,2) - log(total_word, 2)
+        if C_w == 0:
+            return self.lbackoff
+        else:
+            q_uni = C_w / total_word
+
+        if C_vw == 0:
+            return log(self.l3 * q_uni, 2)  # q_bi = 0 if vw is not there uvw won't be there
+        else:
+            q_bi = C_vw / C_v
+
+        if C_uvw == 0:
+            return log(self.l2 * q_bi + self.l3 * q_uni, 2)
+        else:
+            q_tri = C_uvw / C_uv
+
+        return log(self.l1 * q_tri + self.l2 * q_bi + self.l3 * q_uni, 2)
 
     def cond_logprob_laplace(self, word, previous):
 
         UV = previous[-2] + " " + previous[-1]
         UVW = previous[-2] + " " + previous[-1] + " " + word
-        UV = UV.lower()
-        UVW = UVW.lower()
 
-
-        if UVW.lower() in self.trigram:
+        if UVW in self.trigram:
             C_uvw = self.trigram[UVW]
         else:
             C_uvw = 0
@@ -335,27 +326,21 @@ class Trigram(LangModel):
         total_word = len(self.vocab())
         l_v = self.l * total_word
 
-        if l_v == 0 :
-            if C_uv == 0 and C_uvw == 0:
-                score = 0
-            elif C_uvw == 0 :
-                score = self.lbackoff - log((C_uv + l_v), 2)
-            elif C_uv == 0:
-                score = log((C_uvw + self.l), 2) - self.lbackoff
-            else:
-                score = log(C_uvw, 2) - log(C_uv, 2)
+        if l_v == 0 and C_uvw == 0:
+            return self.lbackoff
         else:
-            score = log((C_uvw + self.l), 2) - log((C_uv + l_v), 2)
+            return log((C_uvw + self.l), 2) - log((C_uv + l_v), 2)
 
-        return score
     def vocab(self):
         return self.unigram.keys()
-    def pre_processes(self,corpus):
-        #add <sos>, <sos>, and <eos> to sentence
+
+    def pre_processes(self, corpus):
+        # add <sos>, <sos>, and <eos> to sentence
         for s in corpus:
             s.insert(0, '<SOS>')
             s.insert(0, '<SOS>')
             s.append("<EOS>")
+
     def save_model(self):
         import json
         file = open("dict_trigram", 'w+')
